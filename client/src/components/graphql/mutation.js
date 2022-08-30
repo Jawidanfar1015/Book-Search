22beb104ad234f4d94c77032d1fd7e133dade59a
Jawid Noori <Hajikaka4030@gmail.com>
2022-08-30 00:31:42 -0700

    add user mutation added

--- client/src/components/graphql/mutation.js
+++ client/src/components/graphql/mutation.js
@@ -16,3 +16,32 @@ export const LOGIN_USER = gql`
     }
 `;
 
+export const ADD_USER = gql`
+    mutation addUser( $username: String!, $email: String!, $password: String! )
+    {
+        addUser(
+            username: $usernaem,
+            email: $email,
+            password: $password
+        )
+        {
+            token
+            user {
+                _id
+                username
+                email 
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
+
+` ;
+
